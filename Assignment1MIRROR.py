933e2573c0d9fe848c8f3816d18338bed06eff2d
edyada <94819536+edyada@users.noreply.github.com>
2021-11-22 13:54:16 +0530

    upload assignment1 file mirror
    mirror coded will be uploaded

---
+++ Assignment1MIRROR.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Write a Python program that accepts a word from the user and reverse it.
+
+
+
+Sample Test Case
+
+
+
+Input : Edyoda
+
+output: adoydE
+
+
+# In[2]:
+
+
+word = input("input a word to reverse:")
+
+for char in range(len(word) - 1, -1, -1):
+    print(word[char], end="")
+print("\n")
+
+
+# In[7]:
+
+
+l = ["E","d","y","o","d","a"]
+l[0:5]
+
+
+# In[25]:
+
+
+my_list = ['E','d','y','o','d','a']
+my_list[::-1]
+
+
+# In[27]:
+
+
+l = ["E","d","y","o","d","a"]
+l[::-1]
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+

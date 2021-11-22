26fe2210b4d310442afa807dba174a495c8a71e3
edyada <94819536+edyada@users.noreply.github.com>
2021-11-22 13:43:58 +0530

    assignment1 fabinacii file will be uploaded

---
+++ Assignment1Fabinacii.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Q)Write a Python program to get the Fibonacci series between 0 to 50
+
+
+# In[1]:
+
+
+l = list(range(0,50,2))
+print(l)          
+
+
+# In[2]:
+
+
+l = list(range(0,50,3))
+print(l)          
+
+
+# In[ ]:
+
+
+
+Q)Write a Python program to get the Fibonacci series between 0 to 50
+
+
+Note : The Fibonacci Sequence is the series of numbers :
+
+0, 1, 1, 2, 3, 5, 8, 13, 21, ....
+
+Every next number is found by adding up the two numbers before it.
+
+Expected Output : 1 1 2 3 5 8 13 21 34
+
+
+# In[20]:
+
+
+x = 0
+y = 1
+while y<50:
+    print(y)
+    x, y = y, x+y
+
+
+# In[21]:
+
+
+
+n=int(input("Enter number"))
+x=0
+y=1
+z=0
+while z<=n:
+    print(z)
+    x=y
+    y=z
+    z=x+y
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
